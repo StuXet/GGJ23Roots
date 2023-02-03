167ddcfc62b0f325150fae0439c7f61222304276
StuXet <36419721+StuXet@users.noreply.github.com>
2023-02-03 11:19:48 +0200

    :)

--- GGJ/Assets/Scripts/Enemy.cs
+++ GGJ/Assets/Scripts/Enemy.cs
@@ -8,20 +8,18 @@ public class Enemy : MonoBehaviour
     public float speed = 2f;
 
     private Transform player;
-    private Rigidbody2D rb;
     private RootHealth playerHealth;
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        rb = GetComponent<Rigidbody2D>();
         playerHealth = player.GetComponent<RootHealth>();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         Vector2 direction = (player.position - transform.position).normalized;
-        rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
