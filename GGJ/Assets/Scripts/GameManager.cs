3ae608a1741444341acb464852eef8bfbce327c3
StuXet <36419721+StuXet@users.noreply.github.com>
2023-02-03 16:04:11 +0200

    GG

--- GGJ/Assets/Scripts/GameManager.cs
+++ GGJ/Assets/Scripts/GameManager.cs
@@ -39,7 +39,7 @@ public class GameManager : MonoBehaviour
 
     void UpdateScoreText()
     {
-        scoreText.text = "Score: " + score;
+        scoreText.text = "Points: " + score;
     }
 
     public void Pause()
